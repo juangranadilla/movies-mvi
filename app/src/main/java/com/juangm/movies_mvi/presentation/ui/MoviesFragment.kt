6b15ade831cf4248247c3955ad7d9c17da47a150
juangranadilla <juanillogm91@gmail.com>
2019-07-01 13:22:23 +0200

    Rendering view. Showing movies list, progress and error

--- app/src/main/java/com/juangm/movies_mvi/presentation/ui/MoviesFragment.kt
+++ app/src/main/java/com/juangm/movies_mvi/presentation/ui/MoviesFragment.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.juangm.movies_mvi.R
 import com.juangm.movies_mvi.domain.action.MoviesAction
+import com.juangm.movies_mvi.domain.models.Movie
+import kotlinx.android.synthetic.main.fragment_movies.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class MoviesFragment : Fragment() {
 
     private val moviesViewModel by viewModel<MoviesViewModel>()
+    private lateinit var adapter: MoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,20 +31,50 @@ class MoviesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        moviesViewModel.viewState.observe(this, Observer {
-            render(it)
-        })
+        setRecyclerAdapter()
+        observeMovies()
+        getTopRatedMoviesAction()
+    }
 
+    private fun setRecyclerAdapter() {
+        adapter = MoviesAdapter()
+        movies_recycler.layoutManager = GridLayoutManager(context, 3)
+        movies_recycler.adapter = adapter
+    }
+
+    private fun observeMovies() {
+        moviesViewModel.viewState.observe(this, Observer { moviesViewState ->
+            render(moviesViewState)
+        })
+    }
+
+    private fun getTopRatedMoviesAction() {
         moviesViewModel.dispatch(MoviesAction.GetTopRatedMoviesAction)
     }
 
     private fun render(state: MoviesViewState) {
-        //TODO show loading, movies list or error
+        renderLoadingState(state.isLoading)
+        renderMoviesState(state.movies)
+        renderErrorState(state.error)
+    }
 
-        Timber.i("isLoading: ${state.isLoading}")
-        Timber.i("movies: ${state.movies.size}")
-        state.error?.let {
+    private fun renderLoadingState(isLoading: Boolean) {
+        Timber.i("isLoading: $isLoading")
+        if(isLoading)
+            movies_progress_bar.visibility = View.VISIBLE
+        else
+            movies_progress_bar.visibility = View.GONE
+    }
+
+    private fun renderMoviesState(movies: List<Movie>) {
+        Timber.i("movies: ${movies.size}")
+        adapter.submitList(movies)
+    }
+
+    private fun renderErrorState(error: Throwable?) {
+        error?.let {
             Timber.i("error: $it")
+            Toast.makeText(context, it.localizedMessage, Toast.LENGTH_LONG).show()
         }
     }
 }
