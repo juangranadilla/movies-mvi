79beb98cccf3cd0d7d0e9572715654268e1ec05a
juangranadilla <juanillogm91@gmail.com>
2019-08-13 11:43:46 +0200

    Handling properly previous and actual state, and reducer function

--- domain/src/main/java/com/juangm/domain/usecase/LoadMoreUpcomingMoviesUseCase.kt
+++ domain/src/main/java/com/juangm/domain/usecase/LoadMoreUpcomingMoviesUseCase.kt
@@ -1,8 +1,10 @@
 package com.juangm.domain.usecase
 
 import com.juangm.domain.repository.MoviesRepositoryContract
+import com.juangm.domain.result.MoviesResult
 
 class LoadMoreUpcomingMoviesUseCase(private val moviesRepository: MoviesRepositoryContract): BaseMoviesUseCase() {
 
     override suspend fun getMoviesAsync() = moviesRepository.getUpcomingMoviesAsync(loadMore = true)
+    override val loadingResult = MoviesResult.LoadingMore
 }
--- presentation/src/main/java/com/juangm/presentation/viewmodel/MoviesViewModel.kt
+++ presentation/src/main/java/com/juangm/presentation/viewmodel/MoviesViewModel.kt
@@ -13,54 +13,45 @@ class MoviesViewModel(
     private val loadMorePopularMoviesUseCase: LoadMorePopularMoviesUseCase,
     private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
     private val loadMoreUpcomingMoviesUseCase: LoadMoreUpcomingMoviesUseCase
-): BaseViewModel<MoviesViewState, MoviesAction, MoviesResult>() {
-
-    /**
-     * This is the last view state. We initialize it here, and we will use it in the reduce method,
-     * with the data class copy function, to create the new state, depending on the received result
-     */
-    override var internalViewState = MoviesViewState()
+): BaseViewModel<MoviesViewState, MoviesAction, MoviesResult>(MoviesViewState()) {
 
     /**
      * This is a [BaseViewModel] method we have to implement in every ViewModel.
      * It's the first step, when the ViewModel receives an action (intent)
      * The purpose is to call the proper use case for the required action.
-     * The LiveData block will automatically execute when the LiveData becomes active,
+     * The LiveData block will automatically execute when the LiveData becomes active (just after an action),
      * and it will change its value when the use case calls emit()
      */
     override fun handle(action: MoviesAction) = liveData<MoviesResult> {
         when(action) {
-            is MoviesAction.GetPopularMoviesAction ->
-                if(internalViewState.movies.isEmpty()) getPopularMoviesUseCase.execute(this)
+            is MoviesAction.GetPopularMoviesAction -> getPopularMoviesUseCase.execute(this)
             is MoviesAction.LoadMorePopularMoviesAction -> loadMorePopularMoviesUseCase.execute(this)
-            is MoviesAction.GetTopRatedMoviesAction ->
-                if(internalViewState.movies.isEmpty()) getTopRatedTopRatedMoviesUseCase.execute(this)
+            is MoviesAction.GetTopRatedMoviesAction -> getTopRatedTopRatedMoviesUseCase.execute(this)
             is MoviesAction.LoadMoreTopRatedMoviesAction -> loadMoreTopRatedTopRatedMoviesUseCase.execute(this)
-            is MoviesAction.GetUpcomingMoviesAction ->
-                if(internalViewState.movies.isEmpty()) getUpcomingMoviesUseCase.execute(this)
+            is MoviesAction.GetUpcomingMoviesAction -> getUpcomingMoviesUseCase.execute(this)
             is MoviesAction.LoadMoreUpcomingMoviesAction -> loadMoreUpcomingMoviesUseCase.execute(this)
         }
     }
 
     /**
      * This is a [BaseViewModel] method we have to implement in every ViewModel.
-     * It's the second step, when we receive the result of the use case
-     * The purpose is to create and return a new view state
+     * It's the second step, when we receive the result of the use case and the last view state.
+     * The purpose is to create and return the new view state
      */
-    override fun reduce(result: MoviesResult): MoviesViewState {
+    override fun reduce(currentViewState: MoviesViewState, result: MoviesResult): MoviesViewState {
         return when(result) {
-            is MoviesResult.Loading -> newState(internalViewState.copy(isLoading = true))
-            is MoviesResult.LoadingMore -> newState(internalViewState.copy(isLoading = false, isLoadingMore = true))
-            is MoviesResult.Success -> newState(internalViewState.copy(
+            is MoviesResult.Loading -> currentViewState.copy(isLoading = true)
+            is MoviesResult.LoadingMore -> currentViewState.copy(isLoading = false, isLoadingMore = true)
+            is MoviesResult.Success -> currentViewState.copy(
                 isLoading = false,
                 isLoadingMore = false,
                 movies = result.movies,
-                error = null)
+                error = null
             )
-            is MoviesResult.Failure -> newState(internalViewState.copy(
+            is MoviesResult.Failure -> currentViewState.copy(
                 isLoading = false,
                 isLoadingMore = false,
-                error = Throwable(result.errorMessage))
+                error = Throwable(result.errorMessage)
             )
         }
     }
