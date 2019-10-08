432c78585c010f031a2fdbcb40b060c56ced1383
j.granadilla.manzano <juanillogm91@gmail.com>
2019-10-08 13:49:55 +0200

    Removing Deferred and using suspend in Retrofit service

--- data/src/main/java/com/juangm/data/source/remote/MoviesRemoteSource.kt
+++ data/src/main/java/com/juangm/data/source/remote/MoviesRemoteSource.kt
@@ -6,17 +6,17 @@ import com.juangm.data.source.remote.api.MoviesService
 class MoviesRemoteSource(private val moviesService: MoviesService): MoviesRemoteSourceContract {
 
     override suspend fun getPopularMoviesFromApi(page: Int): MoviesResponse? {
-        val response = moviesService.getPopularMoviesAsync(page = page).await()
+        val response = moviesService.getPopularMoviesAsync(page = page)
         return response.body()
     }
 
     override suspend fun getTopRatedMoviesFromApi(page: Int): MoviesResponse? {
-        val response = moviesService.getTopRatedMoviesAsync(page = page).await()
+        val response = moviesService.getTopRatedMoviesAsync(page = page)
         return response.body()
     }
 
     override suspend fun getUpcomingMoviesFromApi(page: Int): MoviesResponse? {
-        val response = moviesService.getUpcomingMoviesAsync(page = page).await()
+        val response = moviesService.getUpcomingMoviesAsync(page = page)
         return response.body()
     }
 }
--- data/src/main/java/com/juangm/data/source/remote/api/MoviesService.kt
+++ data/src/main/java/com/juangm/data/source/remote/api/MoviesService.kt
@@ -1,7 +1,6 @@
 package com.juangm.data.source.remote.api
 
 import com.juangm.data.BuildConfig
-import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,20 +8,20 @@ import retrofit2.http.Query
 interface MoviesService {
 
     @GET( "movie/popular")
-    fun getPopularMoviesAsync(
+    suspend fun getPopularMoviesAsync(
         @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
         @Query("page") page: Int = 1
-    ): Deferred<Response<MoviesResponse>>
+    ): Response<MoviesResponse>
 
     @GET( "movie/top_rated")
-    fun getTopRatedMoviesAsync(
+    suspend fun getTopRatedMoviesAsync(
         @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
         @Query("page") page: Int = 1
-    ): Deferred<Response<MoviesResponse>>
+    ): Response<MoviesResponse>
 
     @GET( "movie/upcoming")
-    fun getUpcomingMoviesAsync(
+    suspend fun getUpcomingMoviesAsync(
         @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
         @Query("page") page: Int = 1
-    ): Deferred<Response<MoviesResponse>>
+    ): Response<MoviesResponse>
 }
